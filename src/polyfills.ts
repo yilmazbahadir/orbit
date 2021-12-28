06ce40517410e6b114bca710473ff5ce58f7c4e8
Baha <baha@symbol.dev>
2021-12-28 10:25:17 +0000

    fix: runtime dependencies fixed

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -60,3 +60,11 @@ import "zone.js/dist/zone"; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+(window as any).global = window;
+global.Buffer = global.Buffer || require('buffer').Buffer;
+global.process = {
+  env: { DEBUG: undefined },
+  version: '',
+  nextTick: require('next-tick')
+} as any;
